6108dbf5773b2a6afe47d9a044edc38c4f379fc1
Yousuf Iqbal <yousufiqbalhahim@gmail.com>
2022-04-18 10:01:40 +0500

    asdf

--- src/routes/api/accounts/suggest.js
+++ src/routes/api/accounts/suggest.js
@@ -3,9 +3,8 @@ import { knex } from '$lib/database/knex'
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const get = async ({url}) => {
   const keyword = await url.searchParams.get('keyword')
-  if (!keyword) return { status: 200}
+  if (!keyword) return { status: 200 }
   const accounts = await knex('accounts').select('account_id', 'name')
     .where('name', 'like', `%${keyword}%`).limit(5)
-  console.log(accounts)
   return { body: accounts }
 }
